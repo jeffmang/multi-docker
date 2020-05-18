077f42db8c894eaa2880a695ab2b1f0c6fdb8964
Jeff Mangasarian <jeff.mangasarian@focal.systems>
2020-05-18 13:36:43 -0500

    fixed issues with client Dockerfile and app.js

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
             <h1 className="App-title">Welcome to React</h1>
+            <h1>***Hi I'a a React App***</h1>
             <Link to="/">Home</Link>
             <Link to="/otherpage">Other Page</Link>
           </header>
